899a77fb1da816a77cd465f94d5e7073eef428c7
kmusielak218209 <kmusielak218209@e-science.pl>
2019-06-11 20:19:25 +0200

    prototype of bird enemy

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float speed = 0.5f;
+    public float aggroDistance = 2f;
+    private Vector3 startPosition;
+    private Vector3 targetPosition;
+    void Start()
+    {
+        startPosition = transform.position;
+
+    }
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        if (Vector3.Distance(transform.position, FindObjectOfType<Head>().transform.position) < aggroDistance)
+        {
+            targetPosition = FindObjectOfType<Head>().transform.position;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+        }
+        else
+        {
+            targetPosition = startPosition;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+        }
+
+    }
+}
